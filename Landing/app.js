9c7ef2e4f8a28632b377f663bdf839f1be1a36fd
mojtaba-ni <mojtabanik@yahoo.com>
2022-07-03 07:45:22 +0430

    cmt

---
+++ Landing/app.js
@@ -0,0 +1,40 @@
+
+let icon = document.querySelector('.icon')
+let iconActive = document.querySelector('.iconActive')
+let responseLiHeart = document.querySelector('.responseLiHeart')
+let responseLi = document.querySelector('.responseLi')
+let iconMessage = document.querySelector('.iconMessage')
+let iconMessageActive = document.querySelector('.iconMessageActive')
+let inputWrapperNavbar = document.querySelector('.inputWrapperNavbar')
+let scrollToTop = document.querySelector('.scrollToTop')
+
+function HandleIconActive (){
+    responseLiHeart.addEventListener('click', ()=>{
+        icon.style.display = "none";
+        iconActive.style.display = "block";
+    })
+}
+function HandleIconMessageActive (){
+    responseLi.addEventListener('click', ()=>{
+        iconMessage.style.display = "none";
+        iconMessageActive.style.display = "block";
+    })
+}
+
+window.addEventListener('scroll' , (e)=>{
+    if(window.scrollY < 100){
+        inputWrapperNavbar.style.display = 'none'
+        scrollToTop.style.display = 'none'
+    }
+    if(window.scrollY > 100){
+        inputWrapperNavbar.style.display = 'block'
+        scrollToTop.style.display = 'block'
+    }
+})
+
+function scrolTop () {
+    window.scrollTo({top:0 , behavior: 'smooth'})
+}
+
+console.log('window');
+console.log(window)
---
+++ Landing/dropdown.js
@@ -0,0 +1,39 @@
+let arrowClick = document.querySelector('.impTitleMobile')
+let drop = document.querySelector('.dropdownShow')
+let ArrowUp = document.querySelector('.arrowUp')
+let ArrowDown = document.querySelector('.chevronWrapper')
+let searchContainer = document.querySelector('.inputSrch')
+let TitleIc = document.querySelector('.TitleIc')
+
+
+function dropdown () {
+
+    arrowClick.addEventListener('click' , (e)=>{
+        drop.style.display = 'block'
+        ArrowDown.style.display = 'none'
+        ArrowUp.style.display = 'block'
+    })
+}
+
+function dropdownClose () {
+
+    arrowClick.addEventListener('click' , (e)=>{
+        drop.style.display = 'none'
+        ArrowUp.style.display = 'none'
+        ArrowDown.style.display = 'block'
+    })
+}
+
+function dropdownSearch () {
+    TitleIc.addEventListener('click' , (e)=>{
+       
+        searchContainer.style.display = 'block'
+    })
+}
+
+function dropdownCloseSearch () {
+
+    TitleIc.addEventListener('click' , (e)=>{
+        searchContainer.style.display = 'none'
+    })
+}
